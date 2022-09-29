beda1300b118c6838798b32955b46001e62fd429
pikerpoler <53694321+pikerpoler@users.noreply.github.com>
2022-09-29 18:33:21 +0300

    now main is the entry point

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datasets import LyricsOnly
 ds_path = 'datasets/songs'
 
 def main():
-    ds = LyricsOnly(path=ds_path)
+    ds = LyricsOnly(ds_path=ds_path)
+
 if __name__ == '__main__':
     main()
