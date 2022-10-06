ed7999be757eb085530404963c008d75dacc62d5
Yotam Vaknin <yotam@YotamVainsMBP14.lan>
2022-10-06 15:46:45 +0300

    added cosine transform

--- datasets/lyrics_only.py
+++ datasets/lyrics_only.py
@@ -1,15 +1,24 @@
 import torch
+from math import ceil,floor
 from torch.utils.data import Dataset
 from mido import MidiFile, tick2second
 import os
 import numpy as np
+from scipy.fft import dct
 import soundfile as sf
 
 
+OGG_EXT = ".ogg"
 
+
+NUM_SEGMENTS = 2 ** 9
+INT16_MAX = 2**15
+SAMPLE_RATE = 48000
+AUDIO_DURATION_IN_BYTES = 2**18
+AUDIO_DURATION = 5.4613333 # assuming SAMPLERATE=48000 we have AUDIO_DURATION * SAMPLERATE=2**18
 
 class LyricsOnly(Dataset):
-    def __init__(self, ds_path, audio_segment_dir=None, audio_duration=5, ignore_empty=True, split_audio=True, save_songs=True, save_pre_fourier=True, save_fourier=True, rgb_grouping=True, save_collapsed=True):
+    def __init__(self, ds_path, audio_segment_dir=None, audio_duration=AUDIO_DURATION_IN_BYTES, ignore_empty=True, split_audio=True, save_songs=True, save_pre_fourier=True, save_fourier=True, rgb_grouping=True, save_collapsed=True):
         self.ds_path = ds_path
         self.audio_duration = audio_duration
         self.ignore_empty = ignore_empty
@@ -34,67 +43,96 @@ class LyricsOnly(Dataset):
 
 
 
+    def identify_channel(self, filename):
+        channel_type = filename[:-len(OGG_EXT)]
+        return self.track_to_channel[channel_type]
+
 
 
     def _split_audio_to_segments(self):
         for dir in os.listdir(self.ds_path):
             dir_path = os.path.join(self.ds_path, dir)
             if os.path.isdir(dir_path):
+
                 midi_file_path = os.path.join(dir_path, 'notes.mid')
+
                 if not os.path.exists(midi_file_path):
                     continue
+
                 print('working on song: {}'.format(dir))
+
                 t2lyrics = self.cut_lyrics_to_parts(midi_file_path, self.audio_duration)
                 pre_fourier_per_section = dict()
                 fourier_per_section = dict()
+
                 # opening other audio files and cutting them to parts according to lyrics
                 for audio_file in os.listdir(dir_path):
+
                     if audio_file.endswith('.ogg') and audio_file[:-4] in self.track_to_channel:
+
                         audio_file_path = os.path.join(dir_path, audio_file)
-                        audio, samplerate = sf.read(audio_file_path)
+                        audio, samplerate = sf.read(audio_file_path, dtype = np.int16)
+
+                        assert samplerate == SAMPLE_RATE
+
                         for i, ((t_start, t_end), lyrics) in enumerate(t2lyrics.items()):
+
                             # print(f'section {i}: {lyrics}, {len(lyrics)}')
                             if len(lyrics) <= 3 and self.ignore_empty:
                                 continue
+
                             # making sure all directories exist
                             if not os.path.isdir(self.audio_segment_dir):
                                 os.makedirs(self.audio_segment_dir)
                             processed_song_dir = os.path.join(self.audio_segment_dir, dir)
+
                             if not os.path.isdir(processed_song_dir):
                                 os.makedirs(processed_song_dir)
-                            processed_section_dir = os.path.join(processed_song_dir, f'section_{i}_{lyrics}')
+                            processed_section_dir = os.path.join(processed_song_dir, f'section_{i:>02}_{lyrics.strip()}')
+
                             if not os.path.isdir(processed_section_dir):
                                 os.makedirs(processed_section_dir)
-                            audio_section = audio[int(t_start * samplerate):int(t_end * samplerate)]
-                            collapsed_audio_section = np.mean(audio_section, axis=1)  # collapsing L/R to mono
+
+                            section_start = floor(floor(t_start * samplerate))
+                            section_end = section_start + self.audio_duration
+
+                            audio_section = audio[section_start:section_end]
+                            collapsed_audio_section = np.mean(audio_section, axis=1, dtype = np.int16)  # collapsing L/R to mono
+
                             if self.save_songs:
                                 sf.write(os.path.join(processed_section_dir, audio_file), collapsed_audio_section, samplerate)
+
                             if i not in pre_fourier_per_section:
                                 pre_fourier_per_section[i] = np.zeros((len(self.track_to_channel), len(collapsed_audio_section)))
-                            pre_fourier_per_section[i][self.track_to_channel[audio_file[:-4]]] = collapsed_audio_section
+                            pre_fourier_per_section[i][self.identify_channel(audio_file)] = collapsed_audio_section
 
                 for i, (_, lyrics) in enumerate(t2lyrics.items()):
                     if i not in pre_fourier_per_section:
                         continue
 
-                    #################
-                    # TODO: Yotam, this part was suggested by co-pilot, Is it correct?
-                    pre_fourier = pre_fourier_per_section[i]
-                    fourier = np.fft.fft(pre_fourier, axis=1)
-                    fourier_per_section[i] = fourier
-                    #################
+
+                    waveform = pre_fourier_per_section[i]
+
+                    instrument_number, waveform_length = waveform.shape
+                    assert waveform_length == AUDIO_DURATION_IN_BYTES
+
+                    segmented_waveform = waveform.reshape(instrument_number, NUM_SEGMENTS, NUM_SEGMENTS)
+                    cosine_transform = dct(segmented_waveform)
+
+
+                    fourier_per_section[i] = cosine_transform
 
                     if self.save_pre_fourier:
                         processed_song_dir = os.path.join(self.audio_segment_dir, dir)
-                        processed_section_dir = os.path.join(processed_song_dir, f'section_{i}_{lyrics}')
-                        np.save(os.path.join(processed_section_dir, f'pre_fourier.npy'), pre_fourier)
+                        processed_section_dir = os.path.join(processed_song_dir, f'section_{i:>02}_{lyrics.strip()}')
+                        np.save(os.path.join(processed_section_dir, f'pre_fourier.npy'), waveform)
                         if self.save_collapsed:
-                            sf.write(os.path.join(processed_section_dir, 'collapsed.ogg'), pre_fourier.transpose(), samplerate)
+                            sf.write(os.path.join(processed_section_dir, 'collapsed.ogg'), waveform.transpose(), samplerate)
 
                     if self.save_fourier:
                         processed_song_dir = os.path.join(self.audio_segment_dir, dir)
-                        processed_section_dir = os.path.join(processed_song_dir, f'section_{i}_{lyrics}')
-                        np.save(os.path.join(processed_section_dir, f'fourier.npy'), fourier)
+                        processed_section_dir = os.path.join(processed_song_dir, f'section_{i:>02}_{lyrics.strip()}')
+                        np.save(os.path.join(processed_section_dir, f'fourier.npy'), cosine_transform)
 
 
 
@@ -145,6 +183,16 @@ class LyricsOnly(Dataset):
                 start_time = total_time
             i += 1
         return t2lyrics
+
+    @staticmethod
+    def waveform_from_fourier(fourier):
+        """
+        Converts the Fourier representation back to mono waveform
+        """
+
+        assert fourier.shape == NUM_SEGMENTS,NUM_SEGMENTS
+
+        return np.array(np.reshape(fft.idct((fourier)), [-1]), np.int16)
 
 
 
